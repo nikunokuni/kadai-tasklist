971e834bc4d4a41ac310f831e3e1e4491afe100e
maskotto <maskotto@master>
2021-07-20 20:07:01 +0900

    削除実装

--- src/contolollers/DestroyServlet.java
+++ src/contolollers/DestroyServlet.java
@@ -1,11 +1,16 @@
 package contolollers;
 
 import java.io.IOException;
+
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.Task;
+import utils.DBUtil;
 
 /**
  * Servlet implementation class DestroyServlet
@@ -13,29 +18,30 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/destroy")
 public class DestroyServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
     public DestroyServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+	    String _token = request.getParameter("_token");
+        if(_token!=null && _token.equals(request.getSession().getId())) {
+             EntityManager em = DBUtil.createEntityManager();
+//セッションスコープからタスクを探す
+             Task t = em.find(Task.class,(Integer)request.getSession().getAttribute("task_id"));
+             em.getTransaction().begin();
+             em.remove(t);//削除
+             em.getTransaction().commit();
+             em.close();
 
+             request.getSession().removeAttribute("task_id");
+             response.sendRedirect(request.getContextPath()+"/index");
+        }
+	}
 }
--- src/contolollers/UpdateServlet.java
+++ src/contolollers/UpdateServlet.java
@@ -1,11 +1,17 @@
 package contolollers;
 
 import java.io.IOException;
+import java.sql.Timestamp;
+
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.Task;
+import utils.DBUtil;
 
 /**
  * Servlet implementation class UpdateServlet
@@ -13,29 +19,39 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/update")
 public class UpdateServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
     public UpdateServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
+    /**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+	    //CSRF対策
+	    String _token = request.getParameter("_token");
+	    if(_token!=null&&_token.equals(request.getSession().getId())) {
+	         EntityManager em = DBUtil.createEntityManager();
+	         Task t = em.find(Task.class,(Integer)request.getSession().getAttribute("task_id"));
+	         //それぞれ上書き
+	         String content = request.getParameter("content");
+	         t.setContent(content);
 
+	         Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+	         t.setUpdated_at(currentTime);
+
+	         //DB更新
+	         em.getTransaction().begin();
+	         em.getTransaction().commit();
+	         em.close();
+
+	         //不要データの削除
+	         request.getSession().removeAttribute("task_id");
+
+	         //リダイレクト
+	         response.sendRedirect(request.getContextPath()+"/index");
+	    }
+	}
 }
