e8f891c2da6e58b47627a13392dadbdc14fb3f71
maskotto <maskotto@master>
2021-07-20 16:05:06 +0900

    DBテーブル作成完了

--- src/contolollers/IndexServlet.java
+++ src/contolollers/IndexServlet.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.persistence.EntityManager;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -35,15 +34,14 @@ public class IndexServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	    response.getWriter().append("Served at: ").append(request.getContextPath());
 	    EntityManager em = DBUtil.createEntityManager();
 	    List<Task> tasks = em.createNamedQuery("getAllTasks",Task.class).getResultList();
 		response.getWriter().append(Integer.valueOf(tasks.size()).toString());
 		em.close();
 
 		request.setAttribute("tasks",tasks);
-		RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/views/tasks/index.jsp");
-		rd.forward(request, response);
+		//RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/views/tasks/index.jsp");
+		//rd.forward(request, response);
 	}
 
 }
